decd8159b5e01d91db5492967b1fb5d78a7692a6
unknown <Administrator@asvnpnpc43.pn.com.vn>
2019-06-27 18:10:06 +0700

    school management day 2

--- school_management/data.js
+++ school_management/data.js
@@ -1,45 +1,53 @@
 const students = [
-    { "id": "st1", "name": "trang", "address": "Quang Nam", "classId": "1", "phone": "012563" },
-    { "id": "st2", "name": "trang", "address": "Quang Nam", "classId": "3", "phone": "012563" },
-    { "id": "st3", "name": "trang", "address": "Quang Nam", "classId": "1", "phone": "012563" },
-    { "id": "st4", "name": "trang", "address": "Quang Nam", "classId": "1", "phone": "012563" },
-    { "id": "st5", "name": "trang", "address": "Quang Nam", "classId": "1", "phone": "012563" },
-    { "id": "st6", "name": "trang", "address": "Quang Nam", "classId": "2", "phone": "012563" }
+    { "id": "st1", "name": "Nguyen Thi My Trang", "address": "Quang Nam", "classId": "1", "phone": "012563" },
+    { "id": "st2", "name": "Nguyen Thi Thu Huong", "address": "Quang Tri", "classId": "3", "phone": "012563" },
+    { "id": "st3", "name": "Nguyen Thi Phuong Nhung", "address": "Quang Nam", "classId": "1", "phone": "012563" },
+    { "id": "st4", "name": "Nguyen Thanh Nam", "address": "Quang Binh", "classId": "4", "phone": "012563" },
+    { "id": "st5", "name": "Nguyen Van Manh", "address": "Quang Nam", "classId": "1", "phone": "012563" },
+    { "id": "st6", "name": "Nguyen Van Tuan", "address": "Da Nang", "classId": "2", "phone": "012563" }
 ]
 
 const teachers = [
-    { "id": "st1", "name": "trang", "address": "Da Nang", "classId": "Hell", "phone": "012563" },
-    { "id": "st2", "name": "trang", "address": "Da Nang", "classId": "Hell", "phone": "012563" },
-    { "id": "st3", "name": "trang", "address": "Da Nang", "classId": "Hell", "phone": "012563" },
-    { "id": "st4", "name": "trang", "address": "Da Nang", "classId": "Hell", "phone": "012563" },
-    { "id": "st5", "name": "trang", "address": "Da Nang", "classId": "Hell", "phone": "012563" },
-    { "id": "st6", "name": "trang", "address": "Da Nang", "classId": "Hell", "phone": "012563" }
+    { "id": "tc1", "name": "Phan Van Muoi", "address": "Da Nang", "classId": "1", "phone": "012563" },
+    { "id": "tc2", "name": "Pham Tien Duan", "address": "Da Nang", "classId": "2", "phone": "012563" },
+    { "id": "tc3", "name": "Nguyen Quang Ngoc", "address": "Da Nang", "classId": "3", "phone": "012563" },
+    { "id": "tc4", "name": "Nguyen Van Ba", "address": "Da Nang", "classId": "2", "phone": "012563" },
+    { "id": "tc5", "name": "Ho Van Bon", "address": "Da Nang", "classId": "1", "phone": "012563" },
+    { "id": "tc6", "name": "Dao Hai Anh", "address": "Da Nang", "classId": "4", "phone": "012563" }
 ]
 
 const subjects = [
-    { "id": 1, "name": "trang" },
-    { "id": 1, "name": "trang" },
-    { "id": 1, "name": "trang" },
-    { "id": 1, "name": "trang" },
-    { "id": 1, "name": "trang" },
-    { "id": 1, "name": "trang" }
+    { "id": "N2", "name": "English" },
+    { "id": "N3", "name": "Japanese" },
+    { "id": "N4", "name": "Chinese" },
+    { "id": "N5", "name": "French" }
 ]
 
 const scores = [
-    { "subjectId": "trang", "studentId": "PNV1B", "score": 8 },
-    { "subjectId": "trang", "studentId": "PNV1B", "score": 8 },
-    { "subjectId": "trang", "studentId": "PNV1B", "score": 8 },
-    { "subjectId": "trang", "studentId": "PNV1B", "score": 8 },
-    { "subjectId": "trang", "studentId": "PNV1B", "score": 8 },
-    { "subjectId": "trang", "studentId": "PNV1B", "score": 8 }
+    { "subjectId": "N3", "studentId": "st1", "score": 8 },
+    { "subjectId": "N3", "studentId": "st2", "score": 2 },
+    { "subjectId": "N3", "studentId": "st3", "score": 5 },
+    { "subjectId": "N3", "studentId": "st4", "score": 4 },
+    { "subjectId": "N2", "studentId": "st1", "score": 8 },
+    { "subjectId": "N2", "studentId": "st2", "score": 6 },
+    { "subjectId": "N2", "studentId": "st3", "score": 8 },
+    { "subjectId": "N2", "studentId": "st4", "score": 10 },
+    { "subjectId": "N4", "studentId": "st1", "score": 8 },
+    { "subjectId": "N4", "studentId": "st2", "score": 5 },
+    { "subjectId": "N4", "studentId": "st3", "score": 8 },
+    { "subjectId": "N4", "studentId": "st4", "score": 9 },
+    { "subjectId": "N5", "studentId": "st1", "score": 5 },
+    { "subjectId": "N5", "studentId": "st2", "score": 8 },
+    { "subjectId": "N5", "studentId": "st3", "score": 4 },
+    { "subjectId": "N5", "studentId": "st4", "score": 8 }
 ]
 
 const classes = [
-    { "id": 1, "name": "1 A", "teacher": "trang", "quantity": 20 },
-    { "id": 2, "name": "2 A", "teacher": "trang", "quantity": 20 },
-    { "id": 3, "name": "3 A", "teacher": "trang", "quantity": 20 },
-    { "id": 4, "name": "4 A", "teacher": "PNV1B", "quantity": 20 },
-    { "id": 5, "name": "5 A", "teacher": "PNV1B", "quantity": 20 },
+    { "id": "1", "name": "PNV 20B", "teacher": "trang", "quantity": 20 },
+    { "id": "2", "name": "English 3", "teacher": "trang", "quantity": 20 },
+    { "id": "3", "name": "PNV 19A", "teacher": "trang", "quantity": 20 },
+    { "id": "4", "name": "HT Active 20", "teacher": "PNV1B", "quantity": 20 },
+    { "id": "5", "name": "Super Star", "teacher": "PNV1B", "quantity": 20 },
 ]
 
 localStorage.setItem("students", JSON.stringify(students));
--- school_management/school_management.js
+++ school_management/school_management.js
@@ -5,21 +5,57 @@ class school {
         this.scores = JSON.parse(localStorage.getItem("scores"));
         this.subjects = JSON.parse(localStorage.getItem("subjects"));
         this.classes = JSON.parse(localStorage.getItem("classes"));
-        this.loadObject(this.students);
+        this.loadObject(this.students, 'students');
+        this.loadObject(this.teachers, 'teachers');
         this.loadSubject();
+        this.loadScore();
+        this.loadClassAdd();
+        this.loadColumn();
 
     }
+    updateTodo(objectId) {
+        // var currTask = this.tasks.find(t => t.id == idtask).task;
+        let txtName = document.getElementById(objectId + "name");
+        let txtAdd = document.getElementById(objectId + "add");
+        let txtclass = document.getElementById(objectId + "class");
+        let txtphone = document.getElementById(objectId + "phone");
+        txtName.removeAttribute("readonly");
+        txtName.setAttribute("style", "border: 1px solid #800000");
+        txtAdd.removeAttribute("readonly");
+        txtAdd.setAttribute("style", "border: 1px solid #800000");
+        txtclass.removeAttribute("readonly");
+        txtclass.setAttribute("style", "border: 1px solid #800000");
+        txtphone.removeAttribute("readonly");
+        txtphone.setAttribute("style", "border: 1px solid #800000");
+    }
+
+    updateObject(event, objectId) {
+        let txtName = document.getElementById(objectId + "name");
+        let txtAdd = document.getElementById(objectId + "add");
+        let txtclass = document.getElementById(objectId + "class");
+        let txtphone = document.getElementById(objectId + "phone");
+        var ObjectUpdate = this.students.find(ob => ob.id === objectId);
+        ObjectUpdate.name = txtName.value;
+        ObjectUpdate.address = txtAdd.value;
+        ObjectUpdate.classId = txtclass.value;
+        ObjectUpdate.phone = txtphone.value;
+        this.loadObject(this.students, 'students');
+    }
+
     generateObjectHtml(object) {
+        let _className = this.classes.find(cl => cl.id === object.classId).name;
+
         return `
             <tr>
             <td>${object.id}</td>
-            <td>${object.name}</td>
-            <td>${object.address}</td>
-            <td>${object.classId}</td>
-            <td>${object.phone}</td>
-            <td><i class="fa fa-pencil-square-o" aria-hidden="true"></i><i class="fa fa-bars" aria-hidden="true"></i></td>
-        </tr>
-        `;
+            <td> <input id="${object.id}name" style="border: none" readonly value =" ${object.name}" ></td>
+            <td> <input id="${object.id}add" style="border: none" readonly value =" ${object.address}"></td>
+            <td> <input id="${object.id}class" style="border: none" readonly value =" ${_className}"></td>
+            <td> <input id="${object.id}phone" style="border: none" readonly value =" ${object.phone}"></td>
+            <td><i class="fa fa-pencil-square-o" aria-hidden="true"  onClick="sc.updateTodo('${object.id}')"></i>
+            <i onClick="sc.deleteObject(event, '${object.id}')" class="fa fa-trash"  aria-hidden="true" style="listStyleType: none;"> </i>
+            <i class="fa fa-check-square-o" aria-hidden="true" onClick="sc.updateObject(event, '${object.id}')"></i></td>
+        </tr> `;
     }
 
     generateSubjectHtml(subject) {
@@ -27,14 +63,64 @@ class school {
             <tr>
             <td>${subject.id}</td>
             <td>${subject.name}</td>
-            <td><i class="fa fa-pencil-square-o" aria-hidden="true"></i> <i class="fa fa-bars" aria-hidden="true"></i></td>
-        </tr>
-        `;
+            <td><i class="fa fa-pencil-square-o" aria-hidden="true"  ></i>
+            <i  class="fa fa-trash"  aria-hidden="true" style="listStyleType: none;"> </i>
+            <i class="fa fa-check-square-o" aria-hidden="true" ></i></td>
+        </tr>`;
     }
 
-    loadObject(arr) {
+    generateScore(student) {
+        for (let i = 0; i <= this.students.length; i++) {
+            return `
+            <tr>
+                <td>${student[i].studentId[1].name}</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td><i class="fa fa-pencil-square-o" aria-hidden="true"  ></i>
+                <i  class="fa fa-trash"  aria-hidden="true" style="listStyleType: none;"> </i>
+                <i class="fa fa-check-square-o" aria-hidden="true" ></i></td>
+            </tr>`;
+        }
+
+    }
+
+    loadScore() {
+        let arrStu = this.scores.map(sco => ({
+            ...sco,
+            studentId: this.students.filter(s => sco.studentId.includes(s.id)),
+            subjectId: this.subjects.filter(sub => sco.subjectId.includes(sub.id))
+        }))
+        let scoreHtml = arrStu.reduce((html, stu) => (html += this.generateScore(stu)), "");
+        document.getElementById("scores").innerHTML = scoreHtml;
+        console.log("ad", arrStu);
+    }
+
+    generateClassAdd(_class) {
+        return `<option value="${_class.id}">${_class.name}</option>`;
+    }
+
+    generateColumnScore(subject) {
+        return `<th>${subject.name}</th>`;
+    }
+
+    loadColumn() {
+        let colStName = `<th>Student Name</th>`;
+        let colOption = `<th>Option</th>`;
+        let classtHtml = this.subjects.reduce((html, subject) => (html += this.generateColumnScore(subject)), "");
+        document.getElementById('columnScore').innerHTML = colStName + classtHtml + colOption;
+    }
+
+    loadClassAdd() {
+        let classtHtml = this.classes.reduce((html, _class) => (html += this.generateClassAdd(_class)), "");
+        document.getElementById('stClass').innerHTML = classtHtml;
+    }
+
+    loadObject(arr, locate) {
+        localStorage.setItem("students", JSON.stringify(arr));
         let objectHtml = arr.reduce((html, object) => (html += this.generateObjectHtml(object)), "");
-        document.getElementById("person").innerHTML = objectHtml;
+        document.getElementById(locate).innerHTML = objectHtml;
     }
 
     loadSubject() {
@@ -42,15 +128,35 @@ class school {
         document.getElementById("subject").innerHTML = subjectHtml;
     }
 
-    showStudents() {
-        this.loadObject(this.students);
+
+    randomId() {
+        let text = "ST";
+        let possible = "0123456789";
+        for (let i = 0; i < 3; i++)
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        return text;
     }
 
-    showTeachers() {
-        this.loadObject(this.teachers);
-    }
     addNewStudent() {
-        document.getElementById("addStudent").removeAttribute("hidden");
+        let staddr = document.getElementById("stAddress").value;
+        let stname = document.getElementById("stName").value;
+        let stclass = document.getElementById("stClass").value;
+        let stphone = document.getElementById("stPhone").value;
+        let stTemp = {
+            "id": this.randomId(),
+            "name": stname,
+            "address": staddr,
+            "classId": stclass,
+            "phone": stphone
+        };
+        this.students.push(stTemp);
+        this.loadObject(this.students, "students");
+
+        document.getElementById("stAddress").value = "";
+        document.getElementById("stName").value = "";
+        document.getElementById("stClass").value = "";
+        document.getElementById("stPhone").value = "";
+
     }
 
 }
